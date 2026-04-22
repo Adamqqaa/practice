6a4513bb70f29e5ad2a6f85a746ecc5b74a41149
adamqqaa <adamqwertyadam11@gmail.com>
2026-04-22 15:53:20 +0700

    добавил текст филд

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,6 +1,5 @@
 package ci.nsu.moble.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,11 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ci.nsu.moble.main.ui.theme.PracticeTheme
 
@@ -34,7 +37,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+// TODO:  here is to open the second activity
 @Composable
 fun MainScreenActivity(modifier: Modifier = Modifier) {
     var text by remember { mutableStateOf("") }
@@ -44,22 +47,28 @@ fun MainScreenActivity(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // TODO:  нужно добавить  TextField
         TextField(
             value = text,
             onValueChange = { text = it },
             label = { Text("Введите сообщение") },
             modifier = Modifier.fillMaxSize().weight(1f)
         )
-
         Button(
             onClick = {
-                val intent = Intent(context, SecondActivity::class.java)
-                intent.putExtra("USER_MESSAGE", text)
-                context.startActivity(intent)
+                // TODO:  нужно добавить кнопку которая по клику открывает второе активити через интент
             },
             modifier = Modifier.padding(top = 16.dp)
         ) {
             Text("Open SecondActivity")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticeTheme {
+        MainScreenActivity()
     }
 }
