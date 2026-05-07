db0edfe4bb901c69445816982a2dc59aefe406f9
adamqqaa <adamqwertyadam11@gmail.com>
2026-05-08 02:04:16 +0700

    Second commit

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
     implementation(libs.androidx.activity.compose)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.5")
